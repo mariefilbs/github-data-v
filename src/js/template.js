f3c7b43041cb4ea6f2ac9d7bf346a0632449b4ba
Marie Filbey <patriciamariefilbey@gmail.com>
2017-02-21 00:04:57 -0500

    Monday night commit.

--- src/js/template.js
+++ src/js/template.js
@@ -7,15 +7,38 @@ function displayResults (processData) {
   var field = processData;
 
   var basicHTML = `
-    <div class="header">${field.name}</div>
-    <div class="github">${field.html_url}</div>
-    <div class="email">${field.email}</div>
-    <div class="company">${field.company}</div>
-    <div class="blog">${field.blog}</div>
-    <div class="bio">${field.bio}</div>
-    <div class="image">
-    <img src=${field.avatar_url}/>
+    <div class="header">
+    <div id="my-name">${field.name.toLowerCase()}</div>
+      <div class="image">
+        <img src=${field.avatar_url}/>
+      </div>
     </div>
+
+    <div class="mid-content">
+    <div class="name-2">
+      <span class ="basic">Name:</span>
+        ${field.name}
+    </div>
+     <div class="git-hub">
+      <span class ="basic">Github URL:</span>
+        <a href="${field.html_url}">mariefilbs</a>
+     </div>
+    <div class="email">
+      <span class="basic">E-mail:</span>
+        ${field.email}
+    </div>
+    <div class="company">
+      <span class="basic">School:</span>
+        ${field.company}
+    </div>
+    <div class="blog">
+      <span class="basic">Blog:</span>
+        <a href="${field.blog}">Check it out!</a>
+    </div>
+    </div>
+
+    <div class="bio">${field.bio}</div>
+
     `;
 
   $(".basic-results").append(basicHTML);
